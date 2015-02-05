20aad28955ff344a9351c078a604216a6629c040
Ben Lafreniere <ben.lafreniere@gmail.com>
2015-02-05 10:51:37 -0600

    Added the option to define an authentication function on the log route (defaults to no authentication).

--- logroute.js
+++ logroute.js
@@ -13,6 +13,13 @@ function createLogRoute(options) {
     logdir = './data';
   }
 
+  var authFunc;
+  if (options && options.auth) {
+    authFunc = options.auth;
+  } else {
+    authFunc = false;
+  }
+
   var logroute = express.Router();
 
   logroute.use( cors() );
@@ -21,7 +28,14 @@ function createLogRoute(options) {
   logroute.post('/log', function(req, res, next) {
     var user = req.query.user;
     var auth = req.query.auth;
-    // console.log ("%s %s", user, auth);
+
+    if (authFunc && (authFunc(user, auth) === false)) {
+      res.status(403).send({
+	error: "Unauthorized user or authentication."
+      });
+      return;
+    }
+
     if (!req.body) {
       res.status(400).send({
 	error: "Request body was empty!"
